1eaf592ceabd22d601d6d077292de48a139d1b08
nqabenhle <nqmlabajhb024@student.wethinkcode.co.za>
2025-02-25 17:20:37 +0200

    Add link.

--- src/components/heroSection.jsx
+++ src/components/heroSection.jsx
@@ -36,6 +36,8 @@ const HeroSection = () => {
                     <p>Lots of love, Nqabenhle.</p>
                 </div>
                 <button onClick={handleClick} className={confetti && 'button-shaking'}>Press Me! Hooray 🎉</button>
+
+                <div className="gift-location">Find your gift <a href="https://github.com/lindelwa/gift">here</a>.</div>
             </div>
             <div className="shade"></div>
             <div className="image-wrapper">
